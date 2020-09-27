de6b26f5f899cc08497f502ae0fb6564d12cbd60
millisabel <38396309+millisabel@users.noreply.github.com>
2020-09-28 01:08:47 +0300

    created files for main components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.scss';
+import Header from "./components/header/header";
+import Facts from "./components/facts/facts";
+import AirQuality from "./components/airQuality/airQuality";
+import Sectors from "./components/sectors/sectors";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Header/>
+      <Facts/>
+      <AirQuality/>
+      <Sectors/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/sectors/sectors.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sectors = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Sectors;
