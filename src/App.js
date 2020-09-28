327fca2ccbf3c7895d9f369143fafb34fc2ff807
millisabel <38396309+millisabel@users.noreply.github.com>
2020-09-28 11:57:55 +0300

    header desktop scss

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import './App.scss';
+
 import Header from "./components/header/header";
 import Facts from "./components/facts/facts";
 import AirQuality from "./components/airQuality/airQuality";
 import Sectors from "./components/sectors/sectors";
 import Footer from "./components/footer/footer";
 
+import logo1 from './assets/logo/logo_1.svg';
+import logo2 from './assets/logo/logo_2.svg';
+import logo3 from './assets/logo/logo_3.svg';
+
 function App() {
+    const partners = [
+        logo1,
+        logo2,
+        logo3
+    ];
+
+    const navLinks = [
+        {
+            title: "Основные факты",
+            href: "#facts"
+        },
+        {
+            title: "Индекс качества воздуха",
+            href: "#quality"
+        },
+        {
+            title: "Политика по уменьшению загрязнений",
+            href: "#politics"
+        }
+    ];
+
   return (
     <>
-      <Header/>
+      <Header
+          title="Качество атмосферного воздуха и здоровье"
+          logos={partners}
+          links={navLinks}/>
       <Facts/>
       <AirQuality/>
       <Sectors/>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import "./header.scss";
 
-const Header = () => {
+const Header = ({title, logos, links}) => {
+    const logosImages = logos.map((logo, i) => <img src={logo} alt="partner logo" className="header__partners-item" key={i}/>);
+    const navLinks = links.map((link, i) => <a href={link.href} className="header__nav-item" key={i}>{link.title}</a>);
+
     return (
-        <div>
-
-        </div>
+        <section className="header">
+            <div className="container">
+                <div className="header__partners">
+                    {logosImages}
+                </div>
+                <h1 class="header__title">{title}</h1>
+                <div className="header__nav">
+                    {navLinks}
+                </div>
+                <a href="#" className="header__btn">Отследить</a>
+            </div>
+        </section>
     );
 };
 
