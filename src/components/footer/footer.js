e35ab1a9a97482e299af096fc3b9ca84411e2731
millisabel <38396309+millisabel@users.noreply.github.com>
2020-09-28 14:19:36 +0300

    created footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
-const Footer = () => {
+import "./footer.scss";
+
+const Footer = ({links, partners}) => {
+    const partnersLogos = partners.map((logo, i) => <img key={i} src={logo} alt={'partner logo'} className="footer__partners-item"/>);
+    const navLinks = links.map((link, i) => <a href={link.href} key={i} className={'footer__nav-link'}>{link.title}</a>)
+
     return (
-        <div>
-            
-        </div>
+        <footer className="footer">
+            <div className="container">
+                <div className="footer__partners">
+                    {partnersLogos}
+                </div>
+
+                <div className="footer__wrap">
+                    <div className="footer__nav">
+                        {navLinks}
+                    </div>
+
+                    <p className="footer__text">
+                        Сайт был разработан <span>Козловой Еленой</span> в учебных целях, в рамках образовательной программы IT nation. Дизайн сайта создан <span>Валерией Бубырь</span>.
+                    </p>
+                </div>
+
+                <p className="footer__copy">
+                    2020, Все права защищены
+                </p>
+                <p className="footer__copy">
+                    Согласие на обработку персональных данных <br/>
+                    Политика конфиденциальности
+                </p>
+            </div>
+        </footer>
     );
 };
 
