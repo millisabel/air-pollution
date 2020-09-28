c3932b2ddae2e9f6cf20841b94c49ea42bfebe76
millisabel <38396309+millisabel@users.noreply.github.com>
2020-09-28 14:06:28 +0300

    created sectors

--- src/components/sectors/sectors.js
+++ src/components/sectors/sectors.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const Sectors = () => {
-    return (
-        <div>
+import SectorItem from "./sectorsItem";
+import "./sectors.scss"
 
-        </div>
+const Sectors = ({title, subtitle, items}) => {
+    const itemsList = items.map((sector, i) => <SectorItem sector={sector} number={i+1} key={i}/>);
+
+    return (
+        <section className="sectors ">
+            <div className="container">
+                <h2 className="sectors__title">{title}</h2>
+                <p className="sectors__subtitle">{subtitle}</p>
+                <div className="sectors__list">
+                    {itemsList}
+                </div>
+            </div>
+        </section>
     );
 };
 
